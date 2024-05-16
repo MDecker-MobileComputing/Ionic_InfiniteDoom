17cbe980e13ae8aa25d4b2b1b8e3f6e280a258a1
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-16 11:41:40 +0200

    App zeigt jetzt Strings "Schlechte Nachricht <no>" in der Liste an

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'InfiniteDoom',
+  appId: 'de.mide.ionic.infinite-doom',
+  appName: 'Infinite-Doom',
   webDir: 'www'
 };
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public nachrichtenArray: string[] = [];
+
+  ngOnInit() {
+
+    this.nachrichtenErzeugen();
+  }
+
+  private nachrichtenErzeugen() {
+
+    const anzahl = this.nachrichtenArray.length + 1;
+
+    for (let i = 0; i < 15; i++) {
+      const nachricht = `Schlechte Nachricht ${anzahl + i}`;
+      this.nachrichtenArray.push( nachricht );
+    }
+  }
+
+  onIonInfinite( event: InfiniteScrollCustomEvent ) {
+
+    this.nachrichtenErzeugen();
+    setTimeout(() => { event.target.complete(); }, 500);
+  }
 
 }
